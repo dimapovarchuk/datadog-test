4369354039ba5505bf9cda9132868155b6d800ee
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2023-04-06 21:33:57 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 
 # Configure the Datadog provider
 provider "datadog" {
-  api_key = ""
-  app_key = ""
-  api_url = ""
+  api_key = "${var.datadog_api_key}"
+  app_key = "${var.datadog_app_key}"
+  api_url = "${var.datadog_api_url}"
 }
 
 
